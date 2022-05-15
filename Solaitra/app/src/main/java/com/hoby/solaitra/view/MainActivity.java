bf1f1a94e985cc6c4ced9a52ea9550e8b661b5f2
Hobyy19 <arivonyhoby@gmail.com>
2022-05-15 08:12:21 +0300

    Set up Navbar configuration avec utilisation fragment

---
+++ Solaitra/app/src/main/java/com/hoby/solaitra/view/MainActivity.java
@@ -0,0 +1,35 @@
+package com.hoby.solaitra.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hoby.solaitra.R;
+
+import static androidx.navigation.fragment.NavHostFragment.findNavController;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.historiqueFragment, R.id.accueilFragment, R.id.parametresFragment).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+}
--- Solaitra/app/src/main/java/com/hoby/solaitra/view/SplashScreen.java
+++ Solaitra/app/src/main/java/com/hoby/solaitra/view/SplashScreen.java
@@ -1,4 +1,4 @@
-package com.hoby.solaitra;
+package com.hoby.solaitra.view;
 
 import android.content.Intent;
 import android.os.Bundle;
